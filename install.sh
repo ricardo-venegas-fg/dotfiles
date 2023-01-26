242eeaaac795b7d22d0f77d105b74585d88d0b4a
Ricardo Venegas <ricardo.venegas@forgeglobal.com>
2023-01-26 12:49:12 -0600

    Script to setup powershell profile.

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+mkdir --parents /home/vscode/.config/powershell
+
+# line number where payload starts
+PAYLOAD_LINE=$(awk '/^__PAYLOAD_BEGINS__/ { print NR + 1; exit 0; }' $0)
+tail -n +${PAYLOAD_LINE} $0 > /home/vscode/.config/powershell/Microsoft.PowerShell_profile.ps1
+
+exit 0
+
+__PAYLOAD_BEGINS__
+function greenConsoleText($text) {
+    $ESC = [char]27;
+    return "$ESC[32m$($text)$ESC[0m"
+  }
+  
+  function redConsoleText($text) {
+    $ESC = [char]27;
+    return "$ESC[91m$($text)$ESC[0m"
+  }
+   
+  function Prompt {
+    $lastExitCodeBeforePrompt = $global:LASTEXITCODE
+    $new = 0
+    $modified = 0
+    $deleted = 0
+    $prompt = ""
+    $statusList = (git status -s)
+    if ($global:LASTEXITCODE -eq 0) {
+      git status -s | 
+      foreach { $_.Trim().Split(" ")[0] } | 
+      foreach { 
+        if ($_ -eq "??") {
+          $new = $new + 1
+        }
+        else {
+          if ($_ -eq "D") { $deleted = $deleted + 1 }
+          else { $modified = $modified + 1 }
+        }
+      }
+      $currentBranch = (git branch --list | Where { $_.StartsWith("*") }).Split(" ", 2)[1]
+      if ($new -ne 0 -or $modified -ne 0 -or $deleted -ne 0) {
+        $prompt = redConsoleText(" ($currentBranch +$new ~$modified -$deleted)")
+      }
+      else {
+        $prompt = redConsoleText(" ($currentBranch)")
+      }
+    }
+    $global:LASTEXITCODE = $lastExitCodeBeforePrompt
+    "[$(greenConsoleText(get-location))$prompt]`r`nPWSH>"
+  }
+  
+  
+  
